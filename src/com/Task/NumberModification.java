45ce34e38aa4f7fd7c69603391e0f1b68946e538
user <user@DESKTOP-LM7UQL4>
2024-10-10 14:03:17 +0530

    updated NumberModification

---
+++ src/com/Task/NumberModification.java
@@ -0,0 +1,37 @@
+package com.Task;
+
+import java.util.Scanner;
+
+public class NumberModification {
+	
+	int modifyNumber(int num) {
+		int Mof=0;
+		int j=0;
+		String Mstr= Integer.toString(num);
+		StringBuffer sb=new StringBuffer("");
+		int a[]= new int[Mstr.length()];
+		for(int i=0;i<Mstr.length();i++) {
+			a[i]=Mstr.charAt(i)-'0';
+		}
+		for(int i=0;i<Mstr.length()-1;i++) {
+			//System.out.println(Mstr.charAt(i));
+			Mof=Math.abs(a[i+1]-a[i]);
+			sb.insert(j, Mof);
+			j++;
+		}
+		sb.append(a[Mstr.length()-1]);
+		String sb1=sb.toString();
+		int Converted=Integer.parseInt(sb1);
+		return Converted;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the Number:");
+		Scanner sc= new Scanner(System.in);
+
+		int num=sc.nextInt();
+		System.out.println( new NumberModification().modifyNumber(num));
+	}
+
+}
