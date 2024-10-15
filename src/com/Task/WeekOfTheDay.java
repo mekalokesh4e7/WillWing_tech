4b3f37f99f7cf24bebda34211bc9648c91f52057
user <user@DESKTOP-LM7UQL4>
2024-10-15 13:07:29 +0530

    normal

---
+++ src/com/Task/WeekOfTheDay.java
@@ -0,0 +1,15 @@
+package com.Task;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+
+public class WeekOfTheDay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LocalDateTime ldt= LocalDateTime.now();
+		ZonedDateTime zdt= ZonedDateTime.now();
+		
+	}
+
+}
